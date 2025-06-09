3e9ec256919a8539a258a771851e2800463090b6
Rituparna20 <74128140+Rituparna20@users.noreply.github.com>
2025-06-09 14:00:49 +0530

    Update card.tsx

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -1,4 +1,4 @@
-/*import * as React from "react"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -77,4 +77,3 @@ const CardFooter = React.forwardRef<
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
-*/
